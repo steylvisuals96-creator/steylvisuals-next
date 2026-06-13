64c7d0ca628eafc72b2db57f59df15425c55b0ff
sam steylaerts <samsteylaerts@MacBook-Pro-van-sam.local>
2026-06-14 01:38:42 +0200

    Switch showreel video to Hasselt Élégance

--- components/ShowReel.tsx
+++ components/ShowReel.tsx
@@ -116,8 +116,8 @@ export default function ShowReel() {
             }}
           >
             <video
-              src="/videos/web/appartement-sint-truiden.mp4"
-              poster="/videos/web/appartement-sint-truiden.jpg"
+              src="/videos/web/hasselt-elegance.mp4"
+              poster="/videos/web/hasselt-elegance.jpg"
               autoPlay
               muted
               loop
